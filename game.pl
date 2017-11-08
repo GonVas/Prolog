a031cad00e8d3190c737041db7995beb70dd9930
gonvas <gonvalo.moreno97@gmail.com>
2017-11-08 00:01:28 +0000

    Added lots of features

--- game.pl
+++ game.pl
@@ -8,12 +8,12 @@
 moveOneToOther_3(Board, FromTable, TeaToken, NewBoard):-
 	at(CountTable, FromTable, Board),
 	count(CountTable, TeaToken, Total),
-	write('Total is : '), write(Total), nl, 
+%	write('Total for Special is : '), write(Total), nl, 
 	Total > 2,
 
-	write('Do you want to trigger special, move one piece to other table (y/n): '), nl,
-	get_code(Permission),
-	Permission ==  121, % 'y' character
+%	write('Do you want to trigger special, move one piece to other table (y/n): '), nl,
+%	get_code(Permission),
+%	Permission ==  121, % 'y' character
 
 	repeat,
 	write('Triggered Special Move, change on piece to another Table, write first piece: '),
@@ -43,20 +43,50 @@ moveOneToOther_3(Board, FromTable, TeaToken, NewBoard):-
 	get_code(_),
 
 	at(Elem, ToTable, Board),
-	write('Elem is : '), write(Elem), nl,
+%	write('Elem is : '), write(Elem), nl,
 	at(Token, Seat2, Elem),
-	write('Token is : '), write(Token), nl,
+%	write('Token is : '), write(Token), nl,
 	Token == '.',
 
-	write('Going to erasing Tea'), nl,
+%	write('Going to erasing Tea'), nl,
 	eraseTea(Board, FromTable, Seat, NewBoard1),
-	write('Erased Tea'), nl,
+%	write('Erased Tea'), nl,
 	serveTea(NewBoard1, ToTable, Seat2, TeaToken, NewBoard),
-	write('Served Tea'), nl.
+%	write('Served Tea'), nl.
 
 
 moveOneToOther_3(Board,_,_,NewBoard):-
-	write('Couldnt trigger moveOneToOther_3.'), nl,
+	%write('Couldnt trigger moveOneToOther_3.'), nl,
+	assignValue(Board, NewBoard).
+
+
+moveWaiterToOther_4(Board, FromTable, TeaToken, NewBoard):-
+	at(CountTable, FromTable, Board),
+	count(CountTable, TeaToken, Total),
+%	write('Total for Special is : '), write(Total), nl, 
+	Total > 3,
+
+	repeat,
+	write('Triggered Special Move, Move Waiter To other Table: '),
+	get_code(Table1),
+	ToTable is Table1 - 48,
+	ToTable < 9,
+	ToTable >= 0,
+	get_code(_),
+
+
+	at(Elem, ToTable, Board),
+%	write('Elem is : '), write(Elem), nl,
+	find('W', 0, WaiterIndex, Elem),
+	at(Token, WaiterIndex, Elem),
+	Token == '.',
+
+	eraseWaiter(Board, 0, -1, NewBoard2),
+	write('Im here'), nl,
+	moveWaiter(NewBoard2, ToTable, WaiterIndex, NewBoard).
+	
+
+moveWaiterToOther_4(Board, _, _, NewBoard):-
 	assignValue(Board, NewBoard).
 
 eraseTea(Board, Table, Seat, NewBoard) :-
@@ -65,7 +95,7 @@ eraseTea(Board, Table, Seat, NewBoard) :-
 	replace('.', Seat, Elem, NewElem),
 	replace(NewElem, Table, Board, NewBoard).
 
-%==============specials moves, change to specials file========================
+%///==============specials moves, change to specials file========================
 
 createSeats([], M, M). 		%Stop if 2nd args = 3rd arg meaning max elems reached
 %Creates the seats of a table
@@ -91,6 +121,25 @@ moveWaiter(Board, Table, Seat, NewBoard) :-
 
 activateAction(Board, T, P).
 
+/*
+play(Table, Seat, Board) :-
+	at(Elem, Table, Board),
+	count(Elem, '.', TableTotal),
+	write('Table Total : '), write(TableTotal), nl,
+	TableTotal == 0,
+	write('Waiter Stuck, pick new table and seat. New Table: '),
+	
+	get_code(Table1),
+	Table2 is Table1 - 48,
+	Table2 < 9,
+	Table2 >= 0,
+	get_code(_),
+	play(Table2, Seat, Board). 
+*/
+
+
+
+
 % TODO change this fkin repeat
 play(Table, Seat, Board) :-
 	repeat,
@@ -101,7 +150,7 @@ play(Table, Seat, Board) :-
 	get_code(Enter),
 	at(Elem, Table, Board),
 	at(Token, Seat, Elem),
-	Token == '.'.
+	(Token == '.'; Token == 'W').
 	% play(Table, Seat, Board, Token, ValidPosition).
 
 serveTea(Board, Table, Seat, TeaToken, NewBoard) :-
@@ -126,6 +175,7 @@ eraseWaiter(Board, Index, -1, NewBoard) :-
 	eraseWaiter(Board, Index1, WaiterIndex, NewBoard).
 
 turn(TeaToken, Table, Board, NewBoard, NewTable) :-
+	write('Player '), write(TeaToken), write(' turn.'), nl,
 	play(Table, Seat, Board),
 	write(Seat),
 	nl,
@@ -133,31 +183,66 @@ turn(TeaToken, Table, Board, NewBoard, NewTable) :-
 	nl,
 	serveTea(Board, Table, Seat, TeaToken, NewBoard1),
 	eraseWaiter(NewBoard1, 0, -1, NewBoard2),
+
+
+
 	moveWaiter(NewBoard2, Seat, Seat, NewBoard),
 	assignValue(Seat, NewTable),
 	drawBoard(NewBoard).
 
-countMajorTables([], _ , _, _, _).
+%============================Counting Tables ================================
 
-countMajorTables(Board, Init, Total, Token) :- 
-	at(Elem, Init, Board),
- 	countTokenTables(Elem, TableTotal, Token),
-	Init1 is Init+1,
-	countMajorTables(Board, Init1, Total, Token)
+%countTokenTables([], _, _, _, _).
+
+countTokenTables(IsMajor,Total) :-
+	Total > 4,
+	IsMajor = 1.
+
+countTokenTables(IsMajor, Total) :-
+	Total =< 4,
+	IsMajor = 0.
+
+countMajorTables(Board, Max, Total, Token) :-
+%	write('Base Case'), nl,
+	Max < 0.
+
+countMajorTables(Board, Max, Total, Token, NewTotal) :-
+%	write('Recursive Case'), nl,
+	Max >= 0,
+	at(Elem, Max, Board),
+	%write('Elem : ') , write(Elem) , nl,
+	count(Elem, Token, TableTotal),
+	%write('TabelTotal: ') , write(TableTotal) , nl,
+	countTokenTables(IsMajor, TableTotal),
+	%write('Ismajor : ') , write(IsMajor) , nl,
+	Total1 is Total + IsMajor,
+	Max1 is Max-1,
+	%write('Counting tables Total: '), write(Total1), nl,
+	countMajorTables(Board, Max1, Total1, Token, Total1).
+
+%//============================Counting Tables ================================
+
 
 endCondition(Board) :-
-	write('No end'),
-	countMajorTables(Board, 0, Total, Token),
-	write('Total tables is : ') , write(Total), nl,
-	Total > 4,
-	write('We have a winner, congratiolations'), nl.
+	%write('Going to count Major Tables'), nl,
+	countMajorTables(Board, 8, 0, 'X', NewTotal),
+	%write('Total X tables is : ') , write(NewTotal), nl,
+	NewTotal > 4,
+	write('Congratulations Player X you have won.'), nl.
+
+endCondition(Board) :-
+	%write('Going to count Major Tables'), nl,
+	countMajorTables(Board, 8, 0, 'O', NewTotal),
+	%write('Total O tables is : ') , write(NewTotal), nl,
+	NewTotal > 4,
+	write('Congratulations Player O you have won.'), nl.
 
 gameLoop(End, Table, Board) :-
 	endCondition(Board).
 gameLoop(End, Table, Board) :-
 	repeat,
-	turn('X', Table, Board, NewBoard, NewTable),
-	moveOneToOther_3(NewBoard, Table, 'X', NewBoard1), 
+	turn('X', Table, Board, NewBoard1, NewTable),
+	Total is 0,
 	turn('O', NewTable, NewBoard1, NewBoard2, NewTable1),
 	gameLoop(End, NewTable1, NewBoard2).
 
