1d8fe077845fffc9ec3229abfa594538c47699d4
Almeida-Oco <intjalmeida@gmail.com>
2017-12-19 03:42:23 +0000

    Some of the restrictions are working, sum restriction still not working (its currently commented)

---
+++ game.pl
@@ -0,0 +1,77 @@
+:-include('utils.pl').
+:-use_module(library(clpfd)).
+/*
+start([1,1,3], [[4], [1,4], [2], [4]], [[], [3,1], [5], [4]], Result).
+*/
+
+start(SetNumbers, ColRestrictions, RowRestrictions, Result) :-
+	length(ColRestrictions, ColNum),
+	length(RowRestrictions, RowNum),
+	startBoard(ColNum, RowNum, Result),
+	addRowsRest(Result, RowRestrictions, SetNumbers),
+	write('Added Row Restrictions\n'),
+	addColsRest(Result, ColNum, ColRestrictions, SetNumbers),
+	write('Added Column Restrictions\nStarting Labeling!\n'),
+	labelAll(Result).
+
+
+%Instantiates the board with the given number of columns and rows
+startBoard(_, 0, []).
+startBoard(ColNum, RowNum, [RowsHead|RowsTail]) :-
+	startRow(ColNum, RowsHead),
+	domain(RowsHead, -1, 9),
+	Decrement is RowNum - 1,
+	startBoard(ColNum, Decrement, RowsTail).
+startRow(0, []).
+startRow(RowNum, [RowHead|RowTail]) :-
+	RowHead #= _,
+	Decrement is RowNum - 1,
+	startRow(Decrement, RowTail).
+
+%Adds the necessary restrictions to the rows
+addRowsRest([], [], _).
+addRowsRest([Row | BoardRemainder], [RowRest | RestRemainder], SetNumbers) :-
+	write('Iteration '), write(RowRest), nl,
+	generateCardinality(SetNumbers, Cardinality),
+	addListRest(Cardinality, RowRest, Row),
+	addRowsRest(BoardRemainder, RestRemainder, SetNumbers).
+
+%Adds the necessary restrictions to the columns
+addColsRest(Board, ColNumber, ColRests, SetNumbers) :-
+	addColsRest(Board, 0, ColNumber, ColRests, SetNumbers).
+addColsRest(_, End, End, _, _).
+addColsRest(Board, Index, End, [ColRest | RestRemainder], SetNumbers) :-
+	Index < End,
+	Inc is Index + 1,
+	generateCardinality(SetNumbers, Cardinality),
+	nthColumn(Board, Index, Column),
+	addListRest(Cardinality, ColRest, Column),
+	addColsRest(Board, Inc, End, RestRemainder, SetNumbers).
+
+/**
+ * @param SetRest List containing the restrictions of the set, should be of type [num1-ocurrences, num2-ocurrences, -1-ocurrences]
+ * @param ColRest List of type [num1, num2, num3, ...]
+ * @param List A list of type [num1, num2, num3, ...]
+ */
+addListRest([], _).
+addListRest(SetRest, SumRest, List) :-
+	write('Cardinality = '), write(SetRest), nl,nl,
+	global_cardinality(List, SetRest).
+	/*
+	write('After global_cardinality\n'),
+	firstNotOf(List, -1, StartOfList),
+	write('After firstNotOf\n'),
+	splitList(StartOfList, -1, Result),
+	addSumRest(SumRest, Result).
+	*/
+
+%Adds the restriction that list ListHead sum must be equal to RestHead
+addSumRest([], []).
+addSumRest([], [ListHead | ListTail]).
+addSumRest([RestHead | RestTail], [ListHead | ListTail]) :-
+	sumElems(ListHead, Total),
+	Total #= RestHead,
+	% sum(ListHead, #=, RestHead),
+	write('Sum successful\n'),
+	write('addSumRest('), write(RestTail), write(', '), write(ListTail), write(') \n'),
+	addSumRest(RestTail, ListTail).
---
+++ utils.pl
@@ -0,0 +1,74 @@
+:-use_module(library(clpfd)).
+
+
+/**
+ * @brief Generates a list capable of being passed as an argument to global_cardinality
+ * @param SetNumbers The received set numbers from the user
+ * @param StartNumber The number of -1 to put on the end list (corresponds to the number of subdivisions of the list)
+ * 				Can be easily calculated according to the list length of the columns and row restrictions
+ */
+generateCardinality(SetNumbers, [ResHead | ResTail]) :-
+	ResHead = -1-_,
+	generateCardinality(SetNumbers, ResTail, []).
+
+generateCardinality([], [], _).
+generateCardinality([SetNumbersHead|SetNumbersTail], [ResHead|ResTail], Parsed) :-
+	\+ member(SetNumbersHead, Parsed),
+	count([SetNumbersHead|SetNumbersTail], SetNumbersHead, Occurrences),
+	ResHead = SetNumbersHead-Occurrences,
+	append([SetNumbersHead], Parsed, NewParsed),
+	generateCardinality(SetNumbersTail, ResTail, NewParsed).
+generateCardinality([SetNumbersHead|SetNumbersTail], Res, Parsed) :-
+	member(SetNumbersHead, Parsed),
+	generateCardinality(SetNumbersTail, Res, Parsed).
+
+%Counts the number of occurrences of Token in List
+count([], _, 0).
+count([Token | ListTail], Token, Result) :-
+	count(ListTail, Token, Total),
+	Result is Total + 1.
+count([ListHead | ListTail], Token, Result) :-
+	ListHead \= Token,
+	count(ListTail, Token, Total),
+	Result = Total.
+
+%Splits a list using Splitter as the delimiting character.
+splitList([-1|[]], _, [[] | []]).
+splitList([], _, [[] | []]).
+splitList([ListHead | ListTail], Splitter, [[HeadHead | HeadTail] | Tail]) :-
+	ListHead #\= Splitter,
+	HeadHead #= ListHead,
+	splitList(ListTail, Splitter, [HeadTail | Tail]).
+splitList([Splitter | ListTail], Splitter, [Head|Tail]) :-
+	Head = [],
+	splitList(RestOfList, Splitter, Tail).
+
+firstNotOf([ListHead | ListTail], Elem, Result) :-
+	ListHead #\= Elem,
+	Result = [ListHead|ListTail].
+firstNotOf([Elem | ListTail], Elem, Result) :-
+	firstNotOf(ListTail, Elem, Result).
+
+nthElement([], Index, _) :-
+	Index > 0,
+	write('Error nthElement(), accessing '), write(Index), write('element out of bounds!\n'),
+	fail.
+nthElement([Element | _], 0, Element).
+nthElement([_ | ListTail], Index, Element) :-
+	Dec is Index - 1,
+	nthElement(ListTail, Dec, Element).
+
+nthColumn([], _, []).
+nthColumn([Row | Remainder], Index, [ResHead | ResTail]) :-
+	nthElement(Row, Index, ResHead),
+	nthColumn(Remainder, Index, ResTail).
+
+sumElems([], 0).
+sumElems([ListHead|ListTail], Total) :-
+	sumElems(ListTail, Rest),
+	Total #= (ListHead + Rest).
+
+labelAll([]).
+labelAll([ListHead|ListTail]) :-
+	labeling([], ListHead),
+	labelAll(ListTail).
