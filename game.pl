164ffb7bbe982136c6e3a5c95c6f7cf934d299be
Almeida-Oco <intjalmeida@gmail.com>
2017-12-21 03:11:57 +0000

    Most restrictions should be correct, need to check if solutions are correct

--- game.pl
+++ game.pl
@@ -30,90 +30,83 @@ start(SetNumbers, ColRestrictions, RowRestrictions, Result) :-
 	length(ColRestrictions, ColNum),
 	length(RowRestrictions, RowNum),
 	startBoard(ColNum, RowNum, Result), !,
-	addRowsRest(Result, RowRestrictions, SetNumbers, CardinalityLabel1, SumLabel1),
-	addColsRest(Result, 0, ColNum, ColRestrictions, SetNumbers, CardinalityLabel2, SumLabel2),
+	addRowsRest(Result, RowRestrictions, SetNumbers, CardinalityLabel1),
+	addColsRest(Result, 0, ColNum, ColRestrictions, SetNumbers, CardinalityLabel2),
 	append(CardinalityLabel1, CardinalityLabel2, CardinalityLabel),
-	append(SumLabel1, SumLabel2, SumLabel),
-	labelAll(Result, CardinalityLabel, SumLabel, []).
+	labelAll(Result, CardinalityLabel, []).
 
 
 %Adds the necessary restrictions to the rows
-addRowsRest([], [], _, [], _).
-addRowsRest([Row | BoardRemainder], [RowRest | RestRemainder], SetNumbers, [CardHead|CardTail], SumLabel) :-
+addRowsRest([], [], _, []).
+addRowsRest([Row | BoardRemainder], [RowRest | RestRemainder], SetNumbers, [CardHead|CardTail]) :-
 	length(Row, RowSize),
 	length(RowRest, RestSize),
 	generateCardinality(SetNumbers, RestSize, RowSize, Cardinality, CardHead),
-	addListRest(Cardinality, RowRest, Row, SumLabel),
-	addRowsRest(BoardRemainder, RestRemainder, SetNumbers, CardTail, SumLabel).
+	addListRest(Cardinality, RowRest, Row),
+	addRowsRest(BoardRemainder, RestRemainder, SetNumbers, CardTail).
 
 
-addColsRest(_, End, End, _, _, [], _).
-addColsRest(Board, Index, End, [ColRest | RestRemainder], SetNumbers, [CardHead|CardTail], SumLabel) :-
+addColsRest(_, End, End, _, _, []).
+addColsRest(Board, Index, End, [ColRest | RestRemainder], SetNumbers, [CardHead|CardTail]) :-
 	Index < End,
 	Inc is Index + 1,
 	nthColumn(Board, Index, Column),
 	length(Column, ColSize),
 	length(ColRest, RestSize),
 	generateCardinality(SetNumbers, RestSize, ColSize, Cardinality, CardHead),
-	addListRest(Cardinality, ColRest, Column, SumLabel),
-	addColsRest(Board, Inc, End, RestRemainder, SetNumbers, CardTail, SumLabel).
+	addListRest(Cardinality, ColRest, Column),
+	addColsRest(Board, Inc, End, RestRemainder, SetNumbers, CardTail).
 
 
 addListRest([], _, _, _).
-addListRest(SetRest, SumRest, List, SumLabel) :-
+addListRest(SetRest, SumRest, List) :-
 	global_cardinality(List, SetRest),
-	addSumRest(SumRest, List, SumLabel).
+	addSumRest(SumRest, List).
 
 
 %Adds the restriction that list ListHead sum must be equal to RestHead
-addSumRest([], _, _).
-addSumRest(Restrictions, List, SumLabel) :-
+addSumRest([], _).
+addSumRest(Restrictions, List) :-
 	firstNotOf(List, -1, NewList),  % In case list starts in -1
-	addSumRest(Restrictions, NewList, 0, -1, SumLabel).
+	addSumRest(Restrictions, NewList, 0, -1).
 
-% When restrictions are over
-addSumRest([], _, _, _, _).
+% When restrictions are over and list is over
+addSumRest([], [], _, _).
 
-% addSumRest([RestHead | []], [], Total, _, _) :-
-% 	nl, write('Rest Head = '), write(RestHead), nl,
-% 	fail.
-
-% When the list ends
-addSumRest([RestHead | []], [], Total, _,  SumLabel) :-
+% When the list ends case 1
+addSumRest([RestHead | []], [], Total, _) :-
 	RestHead #= Total,
-	SumHead #= Total,
-	SumTail = [],
-	write('\nIm here\n').
+	write('List Ended case 1, tot = '), write(Total), nl.
 
-% When the list ends
-addSumRest([Total | []], [Splitter | []], Total, Splitter,  [Total | []]) :-
-	write('Total '), write(Total), nl.
+% When the list ends case 2
+addSumRest([RestHead | []], [Splitter | []], Total, Splitter) :-
+	RestHead #= Total,
+	write('List Ended case 2, tot = '), write(Total), nl.
 
-addSumRest([Total | RestTail], [Splitter | ListTail], Total, Splitter, [Total|SumTail]) :- %When a splitter is found in the list check its current sum
+addSumRest([Total | RestTail], [Splitter | ListTail], Total, Splitter) :- %When a splitter is found in the list check its current sum
 	write('Total '), write(Total), nl,
 	firstNotOf(ListTail, Splitter, NewListTail),
-	addSumRest(RestTail, NewListTail, 0, Splitter, SumTail).
+	addSumRest(RestTail, NewListTail, 0, Splitter).
 
-addSumRest(Restrictions, [ListHead | ListTail], Total, Splitter, SumLabel) :- % While no splitter is found within the list
+addSumRest(Restrictions, [ListHead | ListTail], Total, Splitter) :- % While no splitter is found within the list
 	Inc #= Total + ListHead,
-	addSumRest(Restrictions, ListTail, Inc, Splitter, SumLabel).
+	addSumRest(Restrictions, ListTail, Inc, Splitter).
 
 
 
 % Add SumLabel to the label list
-labelAll([], [], SumLabel, Result) :-
-	append(Result, SumLabel, NewResult),
+labelAll([], [], Result) :-
 	labeling([], NewResult).
 
 % Add Cardinality Occurrences to the label list
-labelAll([], [CardHead|CardTail], SumLabel, Result) :-
+labelAll([], [CardHead|CardTail], Result) :-
 	append(Result, CardHead, NewResult).
 	% labelAll([], CardTail, SumLabel, NewResult).
 
 % Add board variables to the label list
-labelAll([ListHead|ListTail], Cardinality, SumLabel, Result) :-
+labelAll([ListHead|ListTail], Cardinality, Result) :-
 	append(Result, ListHead, NewResult),
-	labelAll(ListTail, Cardinality, SumLabel, NewResult).
+	labelAll(ListTail, Cardinality, NewResult).
 
 
 % start([1,1,3], [[4], [1,4], [2], [4]], [[], [3,1], [5], [4]], Result).
